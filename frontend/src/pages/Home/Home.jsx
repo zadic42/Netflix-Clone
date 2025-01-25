7f0a7390c6836b690d2e0eadfd9a2084087bc56e
Mohammed sadik <130451993+zadic42@users.noreply.github.com>
2025-01-25 19:22:54 +0530

    Update Home.jsx

--- frontend/src/pages/Home/Home.jsx
+++ frontend/src/pages/Home/Home.jsx
@@ -4,7 +4,7 @@ import Navbar from '../../components/Navbar/Navbar'
 import hero_banner from '../../assets/hero_banner.jpg'
 import hero_title from '../../assets/hero_title.png'
 import play_icon from '../../assets/play_icon.png'
-import info_icon from '../../assets/Info_icon.png'
+import info_icon from '../../assets/info_icon.png'
 import TitleCards from '../../components/TitleCards/TitleCards'
 import Footer from '../../components/Footer/Footer'
 
